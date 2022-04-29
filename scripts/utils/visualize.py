dbdcb08fe755df4fd53e320592ef4662ded2021f
martinez.zacharya <martinez.zacharya@gmail.com>
2022-04-28 22:33:36 -0700

    output plot has interactive legend now

--- scripts/utils/visualize.py
+++ scripts/utils/visualize.py
@@ -10,64 +10,45 @@ from bokeh.models import HoverTool
 hv.extension('bokeh')
 
 def tsne(data, labels):
-	data = np.load('../data/' + str(data) + '.npy')
-	# tsne = PCA(n_components=20).fit_transform(data) #why 20???
-	tsne = PCA(n_components=2).fit_transform(data) #temp for debugging
-	tsne = TSNE(n_components=2).fit_transform(tsne)
-	tsnedf = pd.DataFrame(data = tsne, columns = ['tSNE 1', 'tSNE 2'])
+    data = np.load('../data/' + str(data) + '.npy')
+    tsne = PCA(n_components=20).fit_transform(data) #why 20???
+    tsne = TSNE(n_components=2).fit_transform(tsne)
+    tsnedf = pd.DataFrame(data = tsne, columns = ['tSNE 1', 'tSNE 2'])
 
-	tsnedf = pd.concat([tsnedf, labels], axis = 1)
-
-	return tsnedf
+    tsnedf = pd.concat([tsnedf, labels], axis = 1)
+    
+    return tsnedf
 
 def scatter_viz(df):
 
-	fig = figure(
-		width=800, 
-		height=800, 
-		x_axis_label = 'tSNE 1', 
-		y_axis_label = 'tSNE 2'
-		)
+    fig = figure(
+        width=800, 
+        height=800, 
+        x_axis_label = 'tSNE 1', 
+        y_axis_label = 'tSNE 2'
+    )
+    
+    
+    palette = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000']
+    
+    for label, color in zip((df.Label.unique()), palette):
+        label = str(label)
+        fig.circle(
+            source = df.loc[df['Label'] == label, :],
+            color = color,
+            x = 'tSNE 1',
+            y = 'tSNE 2',
+            legend_label = label
+        )
 
-	fig.circle(
-		source = df.loc[df['Label'] == 1, :],
-		color = '#4daf4a',
-		x = 'tSNE 1', 
-		y = 'tSNE 2', 
-		size = 3
-		)
+    fig.add_tools(
+        HoverTool(
+            tooltips=[("Gene", "@Gene")]
+            )
+        )
+    fig.legend.click_policy="hide"
 
-	fig.circle(
-		source = df.loc[df['Label'] == 0, :],
-		color = '#e41a1c',
-		x = 'tSNE 1', 
-		y = 'tSNE 2', 
-		size = 3
-		)
+    save(fig)
 
-	fig.add_tools(
-		HoverTool(
-			tooltips=[("Gene", "@Gene")]
-			)
-		)
-
-	save(fig)
-	# tsne_plot = hv.Points(
-	# 	data=df,
-	# 	kdims=["tSNE 1", "tSNE 2"],
-	# 	vdims = ['Label']
-	# ).groupby(
-	# 	['Label']
-	# ).overlay(
-	# ).opts(
-	# 	legend_position='right',
-	# 	height=600,
-	# 	width=750,
-	# 	xlabel = "tSNE 1",
-	# 	ylabel = "tSNE 2"
-	# )
-
-	# renderer = hv.renderer('bokeh')
-	# renderer.save(tsne_plot, 'tSNE')
-
-	return True
+    return True
+    
