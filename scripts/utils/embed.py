a85ab6403e55e89101ddcb34b56104f54fc42b7e
martinez.zacharya <martinez.zacharya@gmail.com>
2022-05-22 15:47:29 -0700

    load_state bug

--- scripts/utils/embed.py
+++ scripts/utils/embed.py
@@ -53,12 +53,14 @@ def embed(tuned_model, query, database, name):
 	prs = lambda s: ''.join(s.split('decoder.')[1:] if 'decoder' in s else s)
 	model_args = {pra(arg[0]): arg[1] for arg in vars(model_data["args"]).items()}
 	if tuned_model != 'N' or '':
-		model_state_12 = torch.load(tuned_model)
+		model_t12 = torch.load(tuned_model)
 	else:
 		model_state_12 = {prs(arg[0]): arg[1] for arg in model_data["model"].items()}
 		model_t12.load_state_dict(model_state_12)
+		model_t12 = esm.ProteinBertModel(Namespace(**model_args), len(alphabet), padding_idx=alphabet.padding_idx)
 
-	model_t12 = esm.ProteinBertModel(Namespace(**model_args), len(alphabet), padding_idx=alphabet.padding_idx)
+
+	# model_t12 = esm.ProteinBertModel(Namespace(**model_args), len(alphabet), padding_idx=alphabet.padding_idx)
 
 	q = pd.read_csv(query)
 	db = pd.read_csv(database)
