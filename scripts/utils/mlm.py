5b6fddc4da23744435d86ddd5a90a647216194aa
Zachary Martinez <martinez.zacharya@gmail.com>
2022-06-14 14:06:51 -0700

    new mlm function

---
+++ scripts/utils/mlm.py
@@ -0,0 +1,19 @@
+import torch
+import numpy as np
+import math
+
+def prepare_mlm_mask(alphabet,batch_tokens,mask_frac = 0.15 ,random_subfrac = 0.1, keep_subfrac = 0.1):
+    
+    true_aa = batch_tokens.detach().clone()
+    rand = torch.rand(batch_tokens.shape)
+    mask_arr = (rand < 0.15) * (batch_tokens != 1) * (batch_tokens != 32)
+    selection = []
+
+    for i in range(batch_tokens.shape[0]):
+        selection.append(
+            torch.flatten(mask_arr[i].nonzero()).tolist()
+    )
+    for i in range(batch_tokens.shape[0]):
+        batch_tokens[i, selection[i]] = alphabet.mask_idx
+        
+    return(true_aa, batch_tokens)
