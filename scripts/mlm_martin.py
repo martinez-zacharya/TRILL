e2217215f15bde63fb169b7f54a1ae45828807c2
Zachary Martinez <martinez.zacharya@gmail.com>
2022-09-29 16:51:51 -0700

    overhauling to lightning

--- scripts/mlm_martin.py
+++ scripts/mlm_martin.py
@@ -4,22 +4,21 @@ import numpy as np
 def maskInputs(train_inputs):
 
     # create random array of floats with equal dimensions to input_ids tensor
-    rand = torch.rand(train_inputs['input_ids'].shape)
+    rand = torch.rand(train_inputs.shape).cuda()
     # create mask array
-    mask_arr = (rand < 0.15) * (train_inputs['input_ids'] != 0) * \
-              (train_inputs['input_ids'] != 2) * (train_inputs['input_ids'] != 1)
+    mask_arr = (rand < 0.15) * (train_inputs != 0) * \
+              (train_inputs != 2) * (train_inputs != 1)
 
     selection = []
 
-    for i in range(train_inputs['input_ids'].shape[0]):
+    for i in range(train_inputs.shape[0]):
         selection.append(
             torch.flatten(mask_arr[i].nonzero()).tolist()
         )
     selection[:5]
 
-    for i in range(train_inputs['input_ids'].shape[0]):
-        train_inputs['input_ids'][i, selection[i]] = 32
+    for i in range(train_inputs.shape[0]):
+        train_inputs[i, selection[i]] = 32
 
-    train_inputs['input_ids']
     
     return train_inputs
