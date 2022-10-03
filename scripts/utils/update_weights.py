be0d2d25538f6d2666123aff038fbd92e80e282a
Zachary Martinez <martinez.zacharya@gmail.com>
2022-10-03 11:13:05 -0700

    added func to update weights for lightning model

---
+++ scripts/utils/update_weights.py
@@ -0,0 +1,6 @@
+def weights_update(model, checkpoint):
+    model_dict = model.state_dict()
+    pretrained_dict = {k: v for k, v in checkpoint['state_dict'].items() if k in model_dict}
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict)
+    return model
