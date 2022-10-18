960424235f180d6ef675cf38db279ef782ad68e9
martinez-zacharya <zmartine@login2.cm.cluster>
2022-10-17 20:31:16 -0700

    tokenizer -> self.tokenizer

--- scripts/utils/lightning_models.py
+++ scripts/utils/lightning_models.py
@@ -75,7 +75,7 @@ class ProtGPT2(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         print(batch)
         tokenizer_output = self.tokenizer(batch)
-        tokenizer.pad_token = tokenizer.eos_token
+        self.tokenizer.pad_token = self.tokenizer.eos_token
         data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
         outputs = self.model(tokenizer_output)
         
