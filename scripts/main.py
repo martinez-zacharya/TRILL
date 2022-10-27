e1bc08645f80416969ff3bd096cf4954cc571b0d
martinez-zacharya <zmartine@login2.cm.cluster>
2022-10-27 11:52:34 -0700

    debugging tokenizer/collator

--- scripts/main.py
+++ scripts/main.py
@@ -54,6 +54,7 @@ def main():
             model.tokenizer.pad_token = model.tokenizer.eos_token
             data_collator = DataCollatorForLanguageModeling(model.tokenizer, mlm=False)
             seq_dict = ProtGPT2Dataset(seq_dict)
+            print(seq_dict[0])
             dataloader = torch.utils.data.DataLoader(seq_dict, shuffle = False, batch_size = int(args.batch_size), num_workers=0, collate_fn=data_collator)
         else:
             data = esm.data.FastaBatchedDataset.from_file(args.query)
