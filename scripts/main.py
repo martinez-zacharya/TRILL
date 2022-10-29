2558e552894529862d0719b6bf1e77e37b5c76ff
martinez-zacharya <zmartine@login1.cm.cluster>
2022-10-29 13:56:05 -0700

    debugging protgpt2 tokenizer

--- scripts/main.py
+++ scripts/main.py
@@ -17,7 +17,7 @@ sys.path.insert(0, 'utils')
 from lightning_models import ESM, coordDataset, ProtGPT2, ProtGPT2Dataset
 from update_weights import weights_update
 from datasets import Dataset
-from transformers import DataCollatorForLanguageModeling
+from transformers import DataCollatorForLanguageModeling, AutoTokenizer
 from esm.inverse_folding.util import load_structure, extract_coords_from_structure
 from esm.inverse_folding.multichain_util import extract_coords_from_complex, sample_sequence_in_complex
 
