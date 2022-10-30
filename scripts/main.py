9d038a0c84b0f42fa2dccfa0fe766faf6e5d2483
martinez-zacharya <zmartine@login1.cm.cluster>
2022-10-30 13:24:13 -0700

    debugging protgpt2 tokenizer

--- scripts/main.py
+++ scripts/main.py
@@ -75,7 +75,10 @@ def main():
             out = data_collator([tokenized_datasets["input_ids"][i] for i in range(5)])
             out = ProtGPT2Dataset(out)
             for blah in out:
-                print(blah.shape)
+                for k, v in blah.items():
+                    print(k)
+                    print('-----------')
+                    print(v)
             # blah_list = seq_dict_df['input_ids'].values.tolist()
             # please = model.tokenizer(blah_list, padding = True, return_tensors='pt', return_special_tokens_mask=True)
             dataloader = torch.utils.data.DataLoader(out, shuffle = False, batch_size = int(args.batch_size), num_workers=0)
