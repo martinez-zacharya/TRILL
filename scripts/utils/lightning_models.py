86fc83e571812b5ee5cf83a238d51f0fed6a59ed
martinez-zacharya <zmartine@login2.cm.cluster>
2022-10-30 12:30:58 -0700

    debugging protgpt2 tokenizer

--- scripts/utils/lightning_models.py
+++ scripts/utils/lightning_models.py
@@ -78,7 +78,7 @@ class ProtGPT2(pl.LightningModule):
         # tokenizer_output = self.tokenizer(batch)
         # data_collator = DataCollatorForLanguageModeling(self.tokenizer, mlm=False)
         # collated_data = data_collator(tokenizer_output)
-        outputs = self.model(batch['input_ids'])
+        outputs = self.model(**batch['input_ids'])
         
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=1e5)
