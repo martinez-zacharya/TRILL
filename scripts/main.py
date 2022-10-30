cfca5d2f0b20dfdf2a8e6e76d28168bb90cc199e
martinez-zacharya <zmartine@login1.cm.cluster>
2022-10-30 13:50:14 -0700

    debugging protgpt2 tokenizer

--- scripts/main.py
+++ scripts/main.py
@@ -74,8 +74,10 @@ def main():
             tokenized_datasets = seq_dict_df.map(tokenize, batched=True)
             # out = data_collator([tokenized_datasets["input_ids"][i] for i in range(5)])
             # out = ProtGPT2Dataset(tokenized_datasets)
-            for blah in tokenized_datasets:
-                print(blah)
+            for k, v in tokenized_datasets.items():
+                print(k)
+                print('------')
+                print(v)
                 print('---end-----')
             # blah_list = seq_dict_df['input_ids'].values.tolist()
             # please = model.tokenizer(blah_list, padding = True, return_tensors='pt', return_special_tokens_mask=True)
