b3c89678fb6e8213dba146571014cc42a55332da
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2023-02-07 17:24:03 -0800

    Update strategy_tuner.py

--- trill/utils/strategy_tuner.py
+++ trill/utils/strategy_tuner.py
@@ -90,9 +90,9 @@ def tune_esm_train(data, gpu, billions, strategy):
     else:
         ESM2_list = [
             'esm2_t33_650M_UR50D',
-#             'esm2_t30_150M_UR50D',
-#             'esm2_t12_35M_UR50D',
-#             'esm2_t6_8M_UR50D'
+            'esm2_t30_150M_UR50D',
+            'esm2_t12_35M_UR50D',
+            'esm2_t6_8M_UR50D'
         ]
 
     if strategy == None:
