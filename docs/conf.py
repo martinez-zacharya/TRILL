7d436c6938ad8d161d2709802fefcdec18c43b70
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2023-02-09 12:15:06 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,5 +4,5 @@ project = "TRILL"
 html_title = 'TRILL'
 author = "Zachary A. Martinez"
 sys.path.insert(0, os.path.abspath('..'))
-extensions = ['sphinxarg.ext', 'myst_parser', 'sphinx_rtd_dark_mode']
+extensions = ['sphinxarg.ext', 'myst_parser', 'sphinx_rtd_dark_mode', 'sphinx_copybutton']
 default_dark_mode = True
