796ff46093589c6273966e27c4efb91a50115f8c
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2023-02-15 16:23:25 -0800

    Update trill_main.py

--- trill/trill_main.py
+++ trill/trill_main.py
@@ -456,10 +456,11 @@ def main(args):
             model_import_name = f'esm.pretrained.{args.esm2_arch}()'
             if args.finetuned == True:
                 model = weights_update(model = ESM_Gibbs(eval(model_import_name), 0.0001, False, args.seed_seq, int(args.num_return_sequences), max_len=int(args.max_length), temp=float(args.temp), top_k = int(args.top_k)), checkpoint = torch.load(args.finetuned))
+                tuned_name = args.finetuned.split('/')[-1]            
             else:
                 model = ESM_Gibbs(eval(model_import_name), 0.0001, False, args.seed_seq, int(args.num_return_sequences), max_len=int(args.max_length), temp=float(args.temp), top_k = int(args.top_k))
+                tuned_name = f'esm.pretrained.{args.esm2_arch}___'
             model.predict()
-            tuned_name = args.finetuned.split('/')[-1]
             with open(f'{args.name}_{tuned_name[0:-3]}_Gibbs.fasta', 'w+') as fasta:
                 for i in range(len(model.sample_seqs)):
                     fasta.write(f'>{args.name}_{tuned_name[0:-3]}_Gibbs_{i} \n')
