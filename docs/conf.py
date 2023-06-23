c509346f169213843724a5bf7eddd12e4b608919
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2023-06-23 08:19:28 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,5 +5,5 @@ html_title = 'TRILL'
 author = "Zachary A. Martinez"
 sys.path.insert(0, os.path.abspath('..'))
 extensions = ['sphinxarg.ext', 'myst_parser', 'sphinx_rtd_dark_mode', 'sphinx_copybutton']
-autodoc_mock_imports = ['pytorch_lightning', 'torch', 'pyg_lib']
+autodoc_mock_imports = ['pytorch-lightning', 'torch', 'pyg_lib']
 default_dark_mode = True
