91c607b91ae7333cfe42cddebbacfd91136b1393
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2023-06-24 09:05:55 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,5 +5,5 @@ html_title = 'TRILL'
 author = "Zachary A. Martinez"
 sys.path.insert(0, os.path.abspath('..'))
 extensions = ['sphinxarg.ext', 'myst_parser', 'sphinx_rtd_dark_mode', 'sphinx_copybutton']
-autodoc_mock_imports = ["trill-proteins", "esm", "numpy", "pandas", "torch", "pyg-lib", "torch-scatter", "torch-sparse", "torch-cluster", "torch-spline-conv", "torch-geometric"]
+autodoc_mock_imports = ["trill-proteins", "esm", "fair-esm", "numpy", "pandas", "torch", "pyg-lib", "torch-scatter", "torch-sparse", "torch-cluster", "torch-spline-conv", "torch-geometric"]
 default_dark_mode = True
