dbfbb77bd149c7a509daa4264e1f02f154f7b8b3
Dante Dam <laggycomputer@yahoo.com>
2024-03-29 23:39:07 -0700

    utils: enforce double quotes

--- trill/commands/utils.py
+++ trill/commands/utils.py
@@ -1,10 +1,10 @@
 def setup(subparsers):
-    utils = subparsers.add_parser('utils', help='Misc utilities')
+    utils = subparsers.add_parser("utils", help="Misc utilities")
 
     utils.add_argument(
         "tool",
         help="prepare_class_key: Pepare a csv for use with the classify command. Takes a directory or text file with list of paths for fasta files. Each file will be a unique class, so if your directory contains 5 fasta files, there will be 5 classes in the output key csv.",
-        choices=['prepare_class_key', 'fetch_embeddings']
+        choices=["prepare_class_key", "fetch_embeddings"]
     )
 
     utils.add_argument(
@@ -21,20 +21,20 @@ def setup(subparsers):
     utils.add_argument(
         "--uniprotDB",
         help="UniProt embedding dataset to download.",
-        choices=['UniProtKB',
-                 'A.thaliana',
-                 'C.elegans',
-                 'E.coli',
-                 'H.sapiens',
-                 'M.musculus',
-                 'R.norvegicus',
-                 'SARS-CoV-2'],
+        choices=["UniProtKB",
+                 "A.thaliana",
+                 "C.elegans",
+                 "E.coli",
+                 "H.sapiens",
+                 "M.musculus",
+                 "R.norvegicus",
+                 "SARS-CoV-2"],
         action="store",
     )
     utils.add_argument(
         "--rep",
         help="The representation to download.",
-        choices=['per_AA', 'avg'],
+        choices=["per_AA", "avg"],
         action="store"
     )
 
@@ -45,9 +45,9 @@ def run(args, logger, profiler):
     from trill.utils.classify_utils import generate_class_key_csv
     from trill.utils.fetch_embs import convert_embeddings_to_csv, download_embeddings
 
-    if args.tool == 'prepare_class_key':
+    if args.tool == "prepare_class_key":
         generate_class_key_csv(args)
-    elif args.tool == 'fetch_embeddings':
+    elif args.tool == "fetch_embeddings":
         h5_path = download_embeddings(args)
         h5_name = os.path.splitext(os.path.basename(h5_path))[0]
-        convert_embeddings_to_csv(h5_path, os.path.join(args.outdir, f'{h5_name}.csv'))
+        convert_embeddings_to_csv(h5_path, os.path.join(args.outdir, f"{h5_name}.csv"))
