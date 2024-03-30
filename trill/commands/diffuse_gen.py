799c98e6ca4e0da08ee5b4ca7edcfd4c60243a34
Dante Dam <laggycomputer@yahoo.com>
2024-03-29 20:59:01 -0700

    diffuse_gen: enforce double quotes

--- trill/commands/diffuse_gen.py
+++ trill/commands/diffuse_gen.py
@@ -1,5 +1,5 @@
 def setup(subparsers):
-    diffuse_gen = subparsers.add_parser('diff_gen', help='Generate proteins using RFDiffusion')
+    diffuse_gen = subparsers.add_parser("diff_gen", help="Generate proteins using RFDiffusion")
 
     diffuse_gen.add_argument("--contigs",
                              help="Generate proteins between these sizes in AAs for RFDiffusion. For example, --contig 100-200, will result in proteins in this range",
@@ -78,44 +78,44 @@ def run(args, logger, profiler):
 
     from .commands_common import cache_dir
 
-    # command = "conda install -c dglteam dgl-cuda11.7 -y -S -q".split(' ')
+    # command = "conda install -c dglteam dgl-cuda11.7 -y -S -q".split(" ")
     # subprocess.run(command, check = True)
-    print('Finding RFDiffusion weights... \n')
-    if not os.path.exists((os.path.join(cache_dir, 'RFDiffusion_weights'))):
-        os.makedirs(os.path.join(cache_dir, 'RFDiffusion_weights'))
+    print("Finding RFDiffusion weights... \n")
+    if not os.path.exists((os.path.join(cache_dir, "RFDiffusion_weights"))):
+        os.makedirs(os.path.join(cache_dir, "RFDiffusion_weights"))
 
         commands = [
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt',
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt',
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt',
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt',
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt',
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt',
-            'wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt'
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt",
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt",
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt",
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt",
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt",
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt",
+            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt"
         ]
         for command in commands:
-            if not os.path.isfile(os.path.join(cache_dir, f'RFDiffusion_weights/{command.split("/")[-1]}')):
-                subprocess.run(command.split(' '))
-                subprocess.run(['mv', command.split("/")[-1], os.path.join(cache_dir, 'RFDiffusion_weights')])
+            if not os.path.isfile(os.path.join(cache_dir, f"RFDiffusion_weights/{command.split('/')[-1]}")):
+                subprocess.run(command.split(" "))
+                subprocess.run(["mv", command.split("/")[-1], os.path.join(cache_dir, "RFDiffusion_weights")])
 
-    if not os.path.exists(os.path.join(cache_dir, 'RFDiffusion')):
-        print('Cloning forked RFDiffusion')
-        os.makedirs(os.path.join(cache_dir, 'RFDiffusion'))
-        rfdiff = Repo.clone_from('https://github.com/martinez-zacharya/RFDiffusion',
-                                 os.path.join(cache_dir, 'RFDiffusion/'))
+    if not os.path.exists(os.path.join(cache_dir, "RFDiffusion")):
+        print("Cloning forked RFDiffusion")
+        os.makedirs(os.path.join(cache_dir, "RFDiffusion"))
+        rfdiff = Repo.clone_from("https://github.com/martinez-zacharya/RFDiffusion",
+                                 os.path.join(cache_dir, "RFDiffusion/"))
         rfdiff_git_root = rfdiff.git.rev_parse("--show-toplevel")
-        subprocess.run(['pip', 'install', '-e', rfdiff_git_root])
-        command = f'pip install {rfdiff_git_root}/env/SE3Transformer'.split(' ')
+        subprocess.run(["pip", "install", "-e", rfdiff_git_root])
+        command = f"pip install {rfdiff_git_root}/env/SE3Transformer".split(" ")
         subprocess.run(command)
-        sys.path.insert(0, os.path.join(cache_dir, 'RFDiffusion'))
+        sys.path.insert(0, os.path.join(cache_dir, "RFDiffusion"))
 
     else:
-        sys.path.insert(0, os.path.join(cache_dir, 'RFDiffusion'))
-        git_repo = Repo(os.path.join(cache_dir, 'RFDiffusion'), search_parent_directories=True)
+        sys.path.insert(0, os.path.join(cache_dir, "RFDiffusion"))
+        git_repo = Repo(os.path.join(cache_dir, "RFDiffusion"), search_parent_directories=True)
         rfdiff_git_root = git_repo.git.rev_parse("--show-toplevel")
 
     # if args.sym:
-    #     run_rfdiff((f'{rfdiff_git_root}/config/inference/symmetry.yaml'), args)
+    #     run_rfdiff((f"{rfdiff_git_root}/config/inference/symmetry.yaml"), args)
     # else:
-    #     run_rfdiff((f'{rfdiff_git_root}/config/inference/base.yaml'), args)
-    run_rfdiff((f'{rfdiff_git_root}/config/inference/base.yaml'), args)
+    #     run_rfdiff((f"{rfdiff_git_root}/config/inference/base.yaml"), args)
+    run_rfdiff((f"{rfdiff_git_root}/config/inference/base.yaml"), args)
