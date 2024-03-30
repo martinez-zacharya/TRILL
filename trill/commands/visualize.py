c9da5b4500ef5b582d7175b796928770da20f327
Dante Dam <laggycomputer@yahoo.com>
2024-03-29 23:17:04 -0700

    visualize: whitespace and line length

--- trill/commands/visualize.py
+++ trill/commands/visualize.py
@@ -1,22 +1,26 @@
 def setup(subparsers):
     visualize = subparsers.add_parser('visualize', help='Reduce dimensionality of embeddings to 2D')
 
-    visualize.add_argument("embeddings",
-                           help="Embeddings to be visualized",
-                           action="store"
-                           )
+    visualize.add_argument(
+        "embeddings",
+        help="Embeddings to be visualized",
+        action="store"
+    )
 
-    visualize.add_argument("--method",
-                           help="Method for reducing dimensions of embeddings. Default is PCA",
-                           action="store",
-                           choices=['PCA', 'UMAP', 'tSNE'],
-                           default="PCA"
-                           )
-    visualize.add_argument("--key",
-                           help="Input a CSV, with your group mappings for your embeddings where the first column is the label and the second column is the group to be colored.",
-                           action="store",
-                           default=False
-                           )
+    visualize.add_argument(
+        "--method",
+        help="Method for reducing dimensions of embeddings. Default is PCA",
+        action="store",
+        choices=['PCA', 'UMAP', 'tSNE'],
+        default="PCA"
+    )
+    visualize.add_argument(
+        "--key",
+        help="Input a CSV, with your group mappings for your embeddings where the first column is the label and the "
+             "second column is the group to be colored.",
+        action="store",
+        default=False
+    )
 
 
 def run(args, logger, profiler):
