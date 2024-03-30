e500b30755ce02a0f0daa94236c058be5b9eec1f
Dante Dam <laggycomputer@yahoo.com>
2024-03-29 23:18:14 -0700

    visualize: refactor to new file

--- trill/trill_main.py
+++ trill/trill_main.py
@@ -138,30 +138,11 @@ def main(args):
         "diff_gen",
         "classify",
         "fold",
+        "visualize",
     ]:
         commands[command] = importlib.import_module(f"trill.commands.{command}")
         commands[command].setup(subparsers)
 
-
-##############################################################################################################
-    visualize = subparsers.add_parser('visualize', help='Reduce dimensionality of embeddings to 2D')
-
-    visualize.add_argument("embeddings", 
-        help="Embeddings to be visualized", 
-        action="store"
-        )
-    
-    visualize.add_argument("--method", 
-        help="Method for reducing dimensions of embeddings. Default is PCA", 
-        action="store",
-        choices = ['PCA', 'UMAP', 'tSNE'],
-        default="PCA"
-        )
-    visualize.add_argument("--key", 
-        help="Input a CSV, with your group mappings for your embeddings where the first column is the label and the second column is the group to be colored.", 
-        action="store",
-        default=False
-        )
     
 ##############################################################################################################
     simulate = subparsers.add_parser('simulate', help='Use MD to relax protein structures')
@@ -507,14 +488,7 @@ def main(args):
     else:
 
 
-        if args.command == 'visualize':
-            reduced_df, incsv = reduce_dims(args.name, args.embeddings, args.method)
-            layout = viz(reduced_df, args)
-            bokeh.io.output_file(filename=os.path.join(args.outdir, f'{args.name}_{args.method}_{incsv}.html'), title=args.name)
-            bokeh.io.save(layout, filename=os.path.join(args.outdir, f'{args.name}_{args.method}_{incsv}.html'), title = args.name)
-
-
-        elif args.command == 'dock':
+        if args.command == 'dock':
             ligands = []
             if isinstance(args.ligand, list) and len(args.ligand) > 1:
                 for lig in args.ligand:
