64a6dbeacefd803d65ab1cbb0fc98c53913ac8cf
Dante Dam <laggycomputer@yahoo.com>
2024-03-29 21:15:18 -0700

    diffuse_gen: use wget -O instead of wget then calling mv

--- trill/commands/diffuse_gen.py
+++ trill/commands/diffuse_gen.py
@@ -99,19 +99,18 @@ def run(args, logger, profiler):
     if not os.path.exists((os.path.join(cache_dir, "RFDiffusion_weights"))):
         os.makedirs(os.path.join(cache_dir, "RFDiffusion_weights"))
 
-        commands = (
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt",
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt",
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt",
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt",
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt",
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt",
-            "wget -nc http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt"
+        urls = (
+            "http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt",
+            "http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt",
+            "http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt",
+            "http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt",
+            "http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt",
+            "http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt",
+            "http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt"
         )
-        for command in commands:
-            if not os.path.isfile(os.path.join(cache_dir, "RFDiffusion_weights", command.split('/')[-1])):
-                subprocess.run(command.split(" "))
-                subprocess.run(("mv", command.split("/")[-1], os.path.join(cache_dir, "RFDiffusion_weights")))
+        for url in urls:
+            if not os.path.isfile(os.path.join(cache_dir, "RFDiffusion_weights", url.split('/')[-1])):
+                subprocess.run(("wget", "-nc", url, "-O", os.path.join(cache_dir, "RFDiffusion_weights", url.split("/")[-1])))
 
     if not os.path.exists(os.path.join(cache_dir, "RFDiffusion")):
         print("Cloning forked RFDiffusion")
