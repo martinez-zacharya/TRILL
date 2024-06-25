41c4bc1c8f70e9393ec387a1fb17961e3cb35534
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2024-06-25 13:32:28 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import sys
 import os
 project = "TRILL"
 html_title = 'TRILL'
-author = "Zachary A. Martinez"
+author = "Zachary A Martinez"
 sys.path.insert(0, os.path.abspath('..'))
 extensions = ['sphinxarg.ext', 'myst_parser', 'sphinx_rtd_dark_mode', 'sphinx_copybutton']
 # autodoc_mock_imports = ["trill-proteins", "esm", "fair-esm", "numpy", "pandas", "torch", "openmm", "pyg-lib"]
